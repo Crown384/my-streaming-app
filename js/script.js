8778e919611d413c78be35e96b2cca966e818341
Crown384 <86677683+Crown384@users.noreply.github.com>
2023-12-14 13:36:17 -0800

    reuplaoded app-2 folder again

--- js/script.js
+++ js/script.js
@@ -10,6 +10,19 @@ const progressBar = document.getElementById('progress-bar');
 let isLoopEnabled = false;
 let isDragging = false;
 
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('service-worker.js')
+        .then(registration => {
+          console.log('Service Worker registered with scope:', registration.scope);
+        })
+        .catch(error => {
+          console.error('Service Worker registration failed:', error);
+        });
+    });
+  }
+
 // function handleMouseDown(event) {
 //   event.preventDefault();
 //   isDragging = true;
