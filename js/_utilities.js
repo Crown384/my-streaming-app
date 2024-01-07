db7b0152a1af0616b9be954bb2b1106684be82ae
Crown384 <crownstephen384@gmail.com>
2024-01-07 17:38:26 +0100

    Update _utilities.js

---
+++ js/_utilities.js
@@ -0,0 +1,9 @@
+// Combine mousedown and touchstart events
+document.addEventListener('mousedown', handleLinkFocus);
+document.addEventListener('touchstart', handleLinkFocus);
+
+function handleLinkFocus(event) {
+    if (event.target.tagName === 'A') {
+        event.target.blur(); // Remove focus immediately
+    }
+}
